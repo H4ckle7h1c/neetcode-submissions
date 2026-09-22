d8593b2ab371fffea86805302e54c65847bc8933
Ludovic Peyronnet <56484322+H4ckle7h1c@users.noreply.github.com>
2026-09-22 17:21:25 +0700

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        
+        def backtrack(curr_state: int) -> int:
+            if curr_state == 0:
+                return 1
+            if curr_state < 0:
+                return 0
+            if curr_state in memo:
+                return memo[curr_state]
+            
+            memo[curr_state] =  backtrack(curr_state - 1 ) + backtrack(curr_state - 2)
+            return memo[curr_state]
+        
+        return backtrack(n)
